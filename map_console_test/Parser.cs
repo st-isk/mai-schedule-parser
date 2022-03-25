c1ab59407c60b3e07be48f9b069a975b7dc4f560
st.isk <brysov.alex@yandex.ru>
2022-03-26 01:58:31 +0300

    Parser almost fixed...

--- map_console_test/Parser.cs
+++ map_console_test/Parser.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Text.RegularExpressions;
 using LiteDB;
+using System.IO;
 using System.Threading;
 
 namespace map_console_test
@@ -14,14 +15,18 @@ namespace map_console_test
     {
         private const string pattern_inst = "<option value=\"Институт №(\\d{1,2})\" >";
         private const string pattern_std_year = "<option value=\"(\\d)\" >";
-        //private const string pattern_gr = @"href=""detail\.php\?group=(.{3,5}-.{3,7}-\d{2})";
-        private const string pattern_gr = "href=\"index.php?group=(.{3,5}-.{3,7}-\\d{2})\"";
-        private const string pattern_day = "<div class=\"sc-table-col sc-day-header sc-blue.*?<div class=\"sc-table-col sc-day-header sc-gray";
-        private const string pattern_time = "<div class=\"sc-table-col sc-item-time\">(\\d{2}:\\d{2}) &ndash; (\\d{2}:\\d{2})</div>";
-        private const string pattern_class = "<span class=\"glyphicon glyphicon-map-marker\">&nbsp;</span>(.*?)</div>";
-        private const string pattern_subj = "<span class=\"sc-title\">(.*?)</span>";
+        //private const string pattern_gr = @"href=""detail\.php\?group=(.{3,5}-.{3,7}-\d{2})"; //для старой верстки сайта
+        private const string pattern_gr = "href=\"index\\.php\\?group=(.{3,5}-.{3,7}-\\d{2})\"";
+        //private const string pattern_day = "<div class=\"sc-table-col sc-day-header sc-blue.*?<div class=\"sc-table-col sc-day-header sc-gray"; //для старой верстки сайта
+        private const string pattern_day = "step-icon me-0 me-sm-3 step-icon-soft-primary.*?step-icon me-0 me-sm-3 step-icon-soft-dark";
+        //private const string pattern_time = "<div class=\"sc-table-col sc-item-time\">(\\d{2}:\\d{2}) &ndash; (\\d{2}:\\d{2})</div>";
+        private const string pattern_time = "<li class=\"list-inline-item\">(\\d{2}:\\d{2}) &ndash; (\\d{2}:\\d{2})</li>";
+        //private const string pattern_class = "<span class=\"glyphicon glyphicon-map-marker\">&nbsp;</span>(.*?)</div>";
+        private const string pattern_class = "fad fa-map-marker-alt me-2\"></i>(.*?)</li>";
+        //private const string pattern_subj = "<span class=\"sc-title\">(.*?)</span>";
+        private const string pattern_subj = "<p class=\"mb-2 fw-semi-bold text-dark\">(.*?)<span class=\"text-nowrap\">(.*?)<span";
 
-        public static string Download_old(string link)
+        public static string Download(string link) 
         {
             string page;
 
@@ -35,50 +40,76 @@ namespace map_console_test
             return page;
         }
 
-        /*public static void Get_test_info()
+        private static void Get_groups()
         {
-            var all_groups = Parser.Download("https://mai.ru/education/studies/schedule/groups.php");
-            foreach (Match m in Regex.Matches(all_groups, Parser.pattern_inst, RegexOptions.Singleline))
-            {
-                Console.WriteLine(m.Groups[1].Value);
-            }
-            foreach (Match m in Regex.Matches(all_groups, Parser.pattern_std_year, RegexOptions.Singleline))
-            {
-                Console.WriteLine(m.Groups[1].Value);
-            }
-        }*/
+            var client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            var sw = new StreamWriter("groups_list.txt");
 
-        private static async void Get_groups()
-        {
-            var main_shdl_page = await new WebClient().DownloadStringTaskAsync("https://mai.ru/education/studies/schedule/groups.php");
-            var inst_collection = Regex.Matches(main_shdl_page, Parser.pattern_inst, RegexOptions.Singleline);
-            var year_collection = Regex.Matches(main_shdl_page, Parser.pattern_std_year, RegexOptions.Singleline);
-            foreach (Match m_inst in inst_collection)
+            try
             {
-                foreach (Match m_year in year_collection)
+                Random rndm = new Random();
+
+                string main_shdl_page = client.DownloadString("https://mai.ru/education/studies/schedule/groups.php"); //Качаем главную страницу с расписанием (на ней есть options со всеми институтами и курсами)
+
+                var inst_collection = Regex.Matches(main_shdl_page, Parser.pattern_inst, RegexOptions.Singleline); //Парсим со странички данные об институтах
+                var year_collection = Regex.Matches(main_shdl_page, Parser.pattern_std_year, RegexOptions.Singleline); //Парсим со странички данные о курсах
+                foreach (Match m_inst in inst_collection)
                 {
-                    var inst_year_page = await new WebClient().DownloadStringTaskAsync("https://mai.ru/education/studies/schedule/groups.php?department=Институт+№" + m_inst + "&course=" + m_year);
-                    var gr_collection = Regex.Matches(inst_year_page, Parser.pattern_gr, RegexOptions.Singleline);
-                    foreach (Match m_gr in gr_collection)
+                    foreach (Match m_year in year_collection)
                     {
-                        //Мб сделать список и бахать туда все группы, но чет такой себе варик, скорее просто записывать все это дело в файл
-                        //запись в файл сделать асинхронной
+                        try
+                        {
+                            string inst_year_page = client.DownloadString("https://mai.ru/education/studies/schedule/groups.php?department=Институт+№" + m_inst.Groups[1].Value + "&course=" + m_year.Groups[1].Value); //Бежим по каждому курсу каждого института
+
+                            var gr_collection = Regex.Matches(inst_year_page, Parser.pattern_gr, RegexOptions.Singleline); //Парсим данные о группах на конкретном курсе конкретного института
+                            foreach (Match m_gr in gr_collection)
+                            {
+                                sw.WriteLine(m_gr.Groups[1].Value); //В файл записываем каждую полученную группу 
+                                Console.WriteLine("записал группу в файл");
+                            }
+
+                            Thread.Sleep(rndm.Next(450, 700));
+                        }
+                        catch (Exception inner_ex)
+                        {
+                            Console.WriteLine(inner_ex.Message); 
+                            continue; //Хорошо бы устроить здесь нормальный логгер
+                            // и предусмотреть возможность моментального автоматического перезапуска парсера в случае выдачи ошибки от сервера,
+                            // связанной с количеством и быстротой запросов
+                        }
+
                     }
                 }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (client != null) client.Dispose();
+                if (sw != null) sw.Dispose();
             }
         }
-
-        public static void Get_info(ILiteCollection<SchedulePos> col)
+        
+        public static void Get_info(/*ILiteCollection<SchedulePos> col*/)
         {
             Random rndm = new Random();
-            
-            foreach (Match m in Regex.Matches(all_gr_str, Parser.pattern_gr, RegexOptions.Multiline)) //бежим по всем совпадениям по паттерну групп (иначе говоря, берем название каждой группы и далее вставляем в ссылку для скачивания)
+            //Parser.Get_groups();
+            var sr= new StreamReader("groups_list.txt");
+            string group_str;
+
+            while (!sr.EndOfStream) //бежим по файлу с названиями групп (иначе говоря, берем название каждой группы и далее вставляем в ссылку для скачивания)
             {
                 try
                 {
-                    string group_str = Parser.Download("https://mai.ru/education/schedule/detail.php?group=" + m.Groups[1].Value); //скачиваем страницу конкретной группы
+                    string actl_grp = sr.ReadLine();
+                    if (actl_grp == "") continue;
+
+                    group_str = Parser.Download("https://mai.ru/education/studies/schedule/index.php?group=" + actl_grp); //скачиваем страницу конкретной группы
                     group_str = Regex.Matches(group_str, Parser.pattern_day, RegexOptions.Singleline)[0].Value; //обрезаем полученную строку (удаляем все данные, не относящиеся к текущему дню)
-                    Console.WriteLine(m.Groups[1].Value);
 
                     MatchCollection matches_time = Regex.Matches(group_str, Parser.pattern_time, RegexOptions.Singleline); //находим на странице совпадения по паттерну времени
                     MatchCollection matches_class = Regex.Matches(group_str, Parser.pattern_class, RegexOptions.Singleline); //находим на странице совпадения по паттерну локации
@@ -87,17 +118,17 @@ namespace map_console_test
                     
                     for (int i = 0; i < mtch_cnt; i++) //цикл, в котором записываем данные о времени
                     {
-                        var pos = new SchedulePos(); //экземпляр класса, в который кидаем данные о парах текущей группы 
+                        //var pos = new SchedulePos(); //экземпляр класса, в который кидаем данные о парах текущей группы 
                         Console.WriteLine(matches_time[i].Groups[1].Value + "-" + matches_time[i].Groups[2].Value);
-                        Console.WriteLine(matches_class[i].Groups[1].Value.Trim());
-                        Console.WriteLine(matches_subj[i].Groups[1].Value);
-                        pos.Time_start = matches_time[i].Groups[1].Value;
-                        pos.Time_finish = matches_time[i].Groups[2].Value;
-                        pos.Location = matches_class[i].Groups[1].Value.Trim();
-                        pos.Subject = matches_subj[i].Groups[1].Value;
-                        pos.Group = m.Groups[1].Value;
-                        if (pos.Group == "") continue;
-                        col.Insert(pos); //Добавляем данные в коллекцию (информацию из экземпляра класса в БД)
+                        Console.WriteLine(matches_class[i].Groups[1].Value);
+                        Console.WriteLine(matches_subj[i].Groups[1].Value + matches_subj[i].Groups[2].Value);
+                        //pos.Time_start = matches_time[i].Groups[1].Value;
+                        //pos.Time_finish = matches_time[i].Groups[2].Value;
+                        //pos.Location = matches_class[i].Groups[1].Value;
+                        //pos.Subject = matches_subj[i].Groups[1].Value + matches_subj[i].Groups[2].Value;
+                        //pos.Group = m.Groups[1].Value;
+                        //if (pos.Group == "") continue;
+                        //col.Insert(pos); //Добавляем данные в коллекцию (информацию из экземпляра класса в БД)
                     }
 
                     Console.WriteLine("---");
